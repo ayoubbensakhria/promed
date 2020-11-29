a960ac6268b9dd7bc1edbf956a8ad82a6b5e7d4c
Ayoub <iayoub@yandex.com>
2020-11-29 19:24:52 +0100

    configure database config file

--- public_html/_index.php
+++ public_html/_index.php
@@ -2,9 +2,9 @@
 <h4>Attempting MySQL connection from php...</h4>
 <?php
 // I assume that you've created a new username and password
-$host = 'mysql';
-$user = 'root';
-$pass = 'root';
+$host = 'localhost';
+$user = 'yoctob58_root';
+$pass = '8UbVyRXqIP7C';
 $conn = new mysqli($host, $user, $pass);
 
 if ($conn->connect_error) {
