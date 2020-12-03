b28d9c0549fa63a8678737dd78c3ad06587b350d
Ayoub <bensakhria.ayoub@gmail.com>
2020-12-03 01:03:48 +0100

    admin login link

--- public_html/application/views/userlogin.php
+++ public_html/application/views/userlogin.php
@@ -207,7 +207,7 @@
                                     </form>
                                     <br>
                                     <p><a href="<?php echo site_url('site/ufpassword') ?>"  class="forgot"> <i class="fa fa-key"></i> <?php echo $this->lang->line('forgot_password'); ?></a> | 
-                                    <a a href="<?php echo site_url('site/login') ?>"><?php echo $this->lang->line('admin_login'); ?></a> </p> 
+                                    <a href="<?php echo site_url('site/login') ?>"><?php echo $this->lang->line('admin_login'); ?></a> </p> 
                                 </div>
                             </div>  
                         </div>
